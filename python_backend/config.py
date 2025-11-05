ebe7564e8363e96969af5ee5b8cdb014ee6cab90
Jerry Ron Sunny <jerryronsunny@proton.me>
2025-11-06 02:32:48 +0530

    api key changed

--- python_backend/config.py
+++ python_backend/config.py
@@ -1,4 +1,4 @@
 import os
 
-GEMINI_API_KEY = "AIzaSyCG22aB1LmOdAt93vqgDeR4qY8De8jTing"
+GEMINI_API_KEY = "AIzaSyDXRvs_nhSLrEG4qQtKpabjIAB46-dYCck"
 GEMINI_API_URL = f"https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key={GEMINI_API_KEY}"
